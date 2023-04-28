47712df1e044f58816fddc919351ad225f1e2b99
kandalog <asuasu.tee@gmail.com>
2023-04-28 13:31:25 +0900

    controller作成の雛形作成

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,26 @@
+class QuestionsController < ApplicationController
+  # 質問一覧
+  def show
+  
+  end
+  
+  # 質問詳細
+  def new
+
+  end
+
+  # 質問編集
+  def edit
+
+  end
+  
+  # 質問更新
+  def update
+
+  end
+
+  # 質問削除
+  def delete
+    
+  end
+end
